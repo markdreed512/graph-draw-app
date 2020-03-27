f0ac8b1dbd6b34b466951d1a53d65b6bcb5c4c19
Mark Reed <markdreed512@gmail.com>
2020-03-27 08:18:50 -0500

    delete route

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -21,5 +21,11 @@ router.post('/add', (req,res) => {
         })
 })
 
+router.delete('/:id', (req, res)=>{
+    User.findById(req.params.id)
+        .then(user => user.remove().then(()=>res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}))
+})
+
 
 module.exports = router
