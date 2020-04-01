32aa517582a778939a67d7902afb3425a45ce954
Mark Reed <markdreed512@gmail.com>
2020-04-01 09:14:18 -0500

    SignUpForm component

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
 import '../Assets/styles/LoginModal/LoginModal.css'
+import SignUpForm from './SignUpForm'
 const LoginModal = (props) => {
     const [visible, setVisible] = useState("hidden")
-    const [username, setUsername] = useState("")
-    const [password1, setPassword1] = useState("")
-    const [password2, setPassword2] = useState("")
 
     const openLoginModal = (e) => {
         if (e.target.id === "user-icon") {
@@ -16,27 +14,6 @@ const LoginModal = (props) => {
         }
     }
    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        let data = {
-            "username": username,
-            "password1": password1,
-            "password2": password2
-        }
-        const options = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body:JSON.stringify(data)
-        }
-        fetch('http://localhost:5000/api/Users/add', options)
-            .then((response)=>{
-                console.log(response)
-                setUsername("")
-                setPassword1("")
-                setPassword2("")
-            })
-    }
-    
     const modalStyle = {
         
         visibility: `${visible}`
@@ -45,26 +22,7 @@ const LoginModal = (props) => {
     return (
         <i id="user-icon" className="fa fa-user" onClick={openLoginModal}>
             <div id="modal" style = {modalStyle}>
-                <form onSubmit = {handleSubmit} action="submit">
-                    <label >Sign Up</label>
-                    <input className="input" 
-                            onChange={(e)=> setUsername(e.target.value)} 
-                            value = {username}
-                            type="text" 
-                            required 
-                            placeholder="username"/>
-                    <input className="input" 
-                            onChange={(e)=> setPassword1(e.target.value)} 
-                            value = {password1}
-                            type="password" 
-                            placeholder="password" />
-                    <input className="input" 
-                            onChange={(e)=> setPassword2(e.target.value)} 
-                            value = {password2}
-                            type="password" 
-                            placeholder="password" />
-                    <input className = "button" type="submit" value = "Submit" />
-                </form>
+                <SignUpForm />
             </div>
         </i>
     )
---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState("")
+    const [password1, setPassword1] = useState("")
+    const [password2, setPassword2] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let data = {
+            "username": username,
+            "password1": password1,
+            "password2": password2
+        }
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify(data)
+        }
+        fetch('http://localhost:5000/api/Users/add', options)
+            .then((response)=>{
+                console.log(response)
+                setUsername("")
+                setPassword1("")
+                setPassword2("")
+            })
+    }
+    return (
+        <form onSubmit = {handleSubmit} action="submit">
+                    <label >Sign Up</label>
+                    <input className="input" 
+                            onChange={(e)=> setUsername(e.target.value)} 
+                            value = {username}
+                            type="text" 
+                            required 
+                            placeholder="username"/>
+                    <input className="input" 
+                            onChange={(e)=> setPassword1(e.target.value)} 
+                            value = {password1}
+                            type="password" 
+                            placeholder="password" />
+                    <input className="input" 
+                            onChange={(e)=> setPassword2(e.target.value)} 
+                            value = {password2}
+                            type="password" 
+                            placeholder="password" />
+                    <input className = "button" type="submit" value = "Submit" />
+                </form>
+    )
+}
+
