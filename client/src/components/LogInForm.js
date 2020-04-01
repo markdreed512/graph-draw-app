6e3540281870eb138311024874fcd6ec051efca8
Mark Reed <markdreed512@gmail.com>
2020-04-01 09:53:27 -0500

    loginForm component and toggle

---
+++ client/src/components/LogInForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export default function LoginForm(props) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:5000/api/Users/')
+            .then((response) => {
+                console.log(response)
+            })
+            .then((data) => {
+                console.log(data)
+            })
+    }
+    return (
+        <div onClick={props.onClick}>
+            <p>create an account <button>here</button></p>
+            <form onSubmit={handleSubmit} action="submit">
+                <label >Log In</label>
+                <input className="input"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    type="text"
+                    required
+                    placeholder="username" />
+                <input className="input"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    type="password"
+                    placeholder="password" />
+                <input className="button" type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import '../Assets/styles/LoginModal/LoginModal.css'
 import SignUpForm from './SignUpForm'
+import LogInForm from './LogInForm'
 const LoginModal = (props) => {
     const [visible, setVisible] = useState("hidden")
-
+    const [displayLogin, setDisplayLogin] = useState(true)
     const openLoginModal = (e) => {
         if (e.target.id === "user-icon") {
             if (visible === "hidden") {
@@ -18,11 +19,14 @@ const LoginModal = (props) => {
         
         visibility: `${visible}`
     }
-
+    const toggleForm= (e) => {
+        setDisplayLogin(false)
+        console.log("hi: " + displayLogin)
+    }
     return (
         <i id="user-icon" className="fa fa-user" onClick={openLoginModal}>
             <div id="modal" style = {modalStyle}>
-                <SignUpForm />
+                {displayLogin? <LogInForm onClick={toggleForm}/> : <SignUpForm />}           
             </div>
         </i>
     )
